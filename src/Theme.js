40410ff8205b2aba6b822662a1564e2b05b18a08
michael-williamson <michaeldavid1721@yahoo.com>
2021-11-30 15:44:15 -0600

    added icons to Survivor

--- src/Theme.js
+++ src/Theme.js
@@ -1,15 +1,10 @@
 import { createTheme } from "@mui/material/styles";
 
 export const theme = createTheme({
-  //   palette: {
-  //     primary: {
-  //       main: "#fff",
-  //     },
-  //     secondary: {
-  //       main: "#1976d2",
-  //     },
-  //   },
-  //   text: {
-  //     disabled: "rgba(0 0 0 / 9%)",
-  //   },
+  palette: {
+    primary: {
+      main: "#1976d2",
+      light: "#fff",
+    },
+  },
 });
--- src/components/survivalScoreboardComponents/IndividualSurvivor.js
+++ src/components/survivalScoreboardComponents/IndividualSurvivor.js
@@ -1,16 +1,28 @@
 import React from "react";
 import { Box } from "@mui/system";
+import fur from "../../media/icons/fur.png";
+import meatFood from "../../media/icons/meat_feed.png";
+import caveShelter from "../../media/icons/cave_shelter.png";
+import { theme } from "../../Theme";
 
 export const IndividualSurvivor = (props) => {
   const { survivorNumber } = props;
   return (
     <Box p={2} bgcolor="info.main">
       <Box
-        color="text.disabled"
+        color={theme.palette.primary.light}
         fontSize={30}
         fontWeight="bold"
       >{`Survivor #${survivorNumber}`}</Box>
-      <Box></Box>
+      <Box>
+        <img src={fur} alt="fur" />
+      </Box>
+      <Box>
+        <img src={meatFood} alt="meat" />
+      </Box>
+      <Box>
+        <img src={caveShelter} alt="cave" />
+      </Box>
       <Box></Box>
       <Box></Box>
     </Box>
